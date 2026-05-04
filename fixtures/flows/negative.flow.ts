d49e9338c1753927be83e5663b39a28c0bf17597
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-05-04 16:39:17 +0200

    dodanie definicji klasy NegetiveFlow - konstruktor

--- fixtures/flows/negative.flow.ts
+++ fixtures/flows/negative.flow.ts
@@ -1,2 +1,7 @@
 import { SearchPage, SearchType } from '../pages/search.page';
 import { NegativeAssert } from '../assertions/negative.assert';
+
+export class NegativeFlow {
+  constructor(private searchPage: SearchPage) {}
+
+}
