1af5e8cdf6941a47eaa477478c7bc42a74656b50
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-05-04 16:40:27 +0200

    klasa NegativeFlow - definicja metody searchInvalid

--- fixtures/flows/negative.flow.ts
+++ fixtures/flows/negative.flow.ts
@@ -4,4 +4,19 @@ import { NegativeAssert } from '../assertions/negative.assert';
 export class NegativeFlow {
   constructor(private searchPage: SearchPage) {}
 
+  async searchInvalid(
+    type: SearchType,
+    value: string,
+    expectedRegex: RegExp
+  ): Promise<NegativeAssert> {
+    await this.searchPage.open();
+
+    const apiResponse = await this.searchPage.searchBy(type, value);
+
+    return new NegativeAssert(
+      // apiResponse,
+      this.searchPage.messageLocator,
+      expectedRegex
+    );
+  }
 }
