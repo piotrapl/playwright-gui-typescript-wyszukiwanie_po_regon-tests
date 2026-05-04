31c5c9202a84d4674e9d21915d2dfc5009c21415
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-05-04 16:27:03 +0200

    nowe fikstury c.d. - dodanie deklaracji

--- fixtures/test-fixtures.ts
+++ fixtures/test-fixtures.ts
@@ -3,3 +3,16 @@ import { allure } from 'allure-playwright';
 
 import { SearchPage } from '../pages/search.page';
 import { NegativeFlow } from '../flows/negative.flow';
+
+type Fixtures = {
+  negativeFlow: NegativeFlow;
+  positiveFlow: PositiveFlow;
+};
+
+export const test = base.extend<Fixtures>({
+  negativeFlow: async ({ page }, use) => {
+    const searchPage = new SearchPage(page);
+    const negativeFlow = new NegativeFlow(searchPage);
+
+    await use(negativeFlow);
+  }
