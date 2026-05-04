81ecfc843c9e259ebc3e11b7e14f8a0039fc6445
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-05-04 17:09:52 +0200

    test negatywny dla REGON: dodanie wywołań testów w pętli

--- tests/negative/regon.negative.spec.ts
+++ tests/negative/regon.negative.spec.ts
@@ -3,4 +3,14 @@ import { invalidRegons } from '../../data/invalid-regons';
 
 for (const dataset of invalidRegons) {
 
+    test(`REGON negative search: ${dataset.regon}`, async ({ negativeFlow }) => {
+        const result = await negativeFlow.searchInvalid(
+          'regon',
+          dataset.regon,
+          dataset.expectedRegex
+        );
+
+      await result.assert();
+    });
+
 }
