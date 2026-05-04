074f1c332bfde97a7b686989136a6e7688a15b16
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-05-04 16:53:44 +0200

    search-page: deklaracja typu "rodzaj wyszukiwania" (tzw. unia), definicja nowej klasy

--- pages/search-page.ts
+++ pages/search-page.ts
@@ -1,3 +1,19 @@
 import { Locator, Page } from '@playwright/test';
 import { ENV } from '../utils/env';
 
+export type SearchType = 'regon' | 'nip' | 'krs';
+
+export class SearchPage {
+  constructor(private page: Page) {}
+
+  get messageLocator(): Locator {
+    return this.page.locator('#divInfoKomunikat');
+  }
+
+  get resultsTable(): Locator {
+    return this.page.locator('#divListaJednostek');
+  }
+
+  get resultsRows(): Locator {
+    return this.resultsTable.locator('tr').filter({ hasText: /\S/ });
+  }
