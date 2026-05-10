11fc3deaf07eaf7566c340b76bb5bb338068f23e
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-05-10 15:26:47 +0200

    ponowna zmiana expectedRegex - na właściwą, po wymuszeniu błędu

--- data/invalid-nips.ts
+++ data/invalid-nips.ts
@@ -3,7 +3,7 @@ export const invalidNips = [
     {
         nip: '0755056161',
         description: 'Nieprawidłowy NIP - 10 cyfr, ale nieprawidłowa suma kontrolna',
-        expectedRegex: /numer NX jest nieprawidłowy/i
+        expectedRegex: /numer NIP jest nieprawidłowy/i
     } 
 
 ]
