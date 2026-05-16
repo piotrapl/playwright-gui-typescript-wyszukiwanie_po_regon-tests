d0d3caee214fef7ba92b06bf7cd68e547845b3eb
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-05-16 18:34:37 +0200

    komentarz - opis klasy NegativeAssert

--- assertions/negative.assert.ts
+++ assertions/negative.assert.ts
@@ -1,5 +1,13 @@
 import { expect, Locator } from '@playwright/test';
 
+/* To jest deklaracja klasy NegativeAssert, używanej do asercji negatywnych w testach.
+   pola messageLocator i expectedRegex - są przekazywane do konstruktora,
+
+   gdyby nie były private, byłyby publiczne i mogłyby być modyfikowane z zewnątrz klasy, 
+   co mogłoby prowadzić do niepożądanych skutków ubocznych.
+
+   metoda assert() - sprawdza, czy tekst z messageLocator zawiera się w expectedRegex.
+*/
 export class NegativeAssert {
   constructor(
     //private apiResponse: ApiResponse,
