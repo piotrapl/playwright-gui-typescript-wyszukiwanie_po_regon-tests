88cbb75ff1a2fb7709659121d59099d1fd1c7c88
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-05-17 17:16:35 +0200

    dodanie kolejnej danej testowej - blędnego REGONu

--- data/invalid-regons.ts
+++ data/invalid-regons.ts
@@ -4,6 +4,11 @@ export const invalidRegons = [
         regon: '12345678   ',
         description: 'Liczba znaków w polu REGON (8) jest inna niż 9 lub 14',
         expectedRegex: /długość|liczba|zawiera|zawierać|.*9.*14|znaków/i
-    } 
+    },
+    {
+      title: 'Liczba znaków w polu REGON (10) jest inna niż 9 lub 14',
+      regon: '1234567891',
+      expectedRegex: /długość|liczba|zawiera|zawierać|.*9.*14|znaków/i
+    }
 
 ]
