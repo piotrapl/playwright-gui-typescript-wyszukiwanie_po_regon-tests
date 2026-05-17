91cb985dc6fff3813bd70c38fa874d733a2270b8
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-05-17 18:46:31 +0200

    opis testu negatywnego dla REGON-u c.d.

--- tests/negative/regon.negative.spec.ts
+++ tests/negative/regon.negative.spec.ts
@@ -2,12 +2,13 @@ import { test } from '../../fixtures/test-fixtures';
 import { invalidRegons } from '../../data/invalid-regons';
 
 /*
-To jest test negatywny dla wyszukiwania REGON. 
+To jest test negatywny dla wyszukiwania REGON.
+pierwszy parametr to opis testu, który będzie wyświetlany np. w raportach
 */
 for (const dataset of invalidRegons) {
 
-    test(`REGON negative search: ${dataset.regon}`, async ({ negativeFlow }) => {
-        const result = await negativeFlow.searchInvalid(
+    test(`Test negetywny wyszukiwania po REGON: ${dataset.regon}`, async ({ negativeFlow }) => {
+        const result = await negativeFlow.searchInvalid(          
           'regon',
           dataset.regon,
           dataset.expectedRegex
