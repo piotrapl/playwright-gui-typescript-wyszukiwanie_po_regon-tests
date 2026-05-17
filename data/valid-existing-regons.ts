85307596f6e97c2626a8785a90510aa240fa8ed9
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-05-17 18:35:20 +0200

    koleny REGON do testów pozytywnych - 14-cyfrowy

--- data/valid-existing-regons.ts
+++ data/valid-existing-regons.ts
@@ -3,5 +3,10 @@ export const validExistingRegons = [
       title: 'prawidłowy REGON 9-cyfrowy',
       regon: '350637551',
       expectedCompanyRegex: /RADIO MUZYKA FAKTY GRUPA RMF/i
+    },
+    {
+      title: 'prawidłowy REGON 14-cyfrowy',
+      regon: '01041897300057',
+      expectedCompanyRegex: /TELEWIZJA POLSKA.*RZESZOWIE|TELEWIZJA POLSKA.*RZESZÓW/i
     }
 ]
