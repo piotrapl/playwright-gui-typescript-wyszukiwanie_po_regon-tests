7487dfb2dffa359942138fdfa760f58b5614c862
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-05-17 18:34:18 +0200

    dodanie prawidłowego REGONu, w sys. GUS powiązanegoz podmiotem

--- data/valid-existing-regons.ts
+++ data/valid-existing-regons.ts
@@ -1,1 +1,7 @@
 export const validExistingRegons = [
+    {
+      title: 'prawidłowy REGON 9-cyfrowy',
+      regon: '350637551',
+      expectedCompanyRegex: /RADIO MUZYKA FAKTY GRUPA RMF/i
+    }
+]
