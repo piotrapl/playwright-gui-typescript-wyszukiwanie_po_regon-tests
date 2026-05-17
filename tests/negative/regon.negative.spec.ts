287da596de59f2b36bf573873d6325eb28f987de
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-05-17 18:42:59 +0200

    komentarz nt. testu negatywnego dla REGON

--- tests/negative/regon.negative.spec.ts
+++ tests/negative/regon.negative.spec.ts
@@ -1,6 +1,9 @@
 import { test } from '../../fixtures/test-fixtures';
 import { invalidRegons } from '../../data/invalid-regons';
 
+/*
+To jest test negatywny dla wyszukiwania REGON. 
+*/
 for (const dataset of invalidRegons) {
 
     test(`REGON negative search: ${dataset.regon}`, async ({ negativeFlow }) => {
@@ -14,3 +17,4 @@ for (const dataset of invalidRegons) {
     });
 
 }
+
