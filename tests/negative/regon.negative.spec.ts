e81e16ec4787ec33c0d85082824f2164f58db772
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-05-17 18:49:19 +0200

    opis parametrów metody test

--- tests/negative/regon.negative.spec.ts
+++ tests/negative/regon.negative.spec.ts
@@ -3,7 +3,10 @@ import { invalidRegons } from '../../data/invalid-regons';
 
 /*
 To jest test negatywny dla wyszukiwania REGON.
-pierwszy parametr to opis testu, który będzie wyświetlany np. w raportach
+1-szy parametr to opis testu, który będzie wyświetlany np. w raportach
+2-gi parametr:   async ({ negativeFlow }) => {
+       to jest funkcja testowa, która jest asynchroniczna (async) i przyjmuje obiekt z różnymi "flow"ami, 
+       - tutaj z negativeFlow - instancją klasy NegativeFlow.
 */
 for (const dataset of invalidRegons) {
 
